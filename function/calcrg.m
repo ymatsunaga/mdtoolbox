59157865886df41ebf6e40d75675c35edf0c70c1
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2013-01-11 01:27:29 +0900

    add calcrg.m (calc radius of gyration)

---
+++ function/calcrg.m
@@ -0,0 +1,36 @@
+function rg = calcrg(trj, mass)
+%% calcrg
+% calc radius of gyration
+% 
+% example: 
+% parm = readparm('ak.parm');
+% trj  = readnetcdf('ak.nc');
+% index_noh = ~selectname(parm.atom_name, 'H*');
+% mass = parm.mass(index_noh);
+% trj  = trj(:, to3(index_noh));
+% rg   = calcrg(trj, mass);
+% 
+
+%% setup
+nstep  = size(trj, 1);
+natom3 = size(trj, 2);
+natom  = natom3/3;
+
+if (nargin < 2) | (numel(mass) == 0)
+  mass = ones(1, natom);
+else
+  if iscolumn(mass)
+    mass = mass';
+  end
+end
+
+%% calculation
+rg = zeros(nstep, 1);
+[trj, com] = decenter(trj, [], mass);
+totalMass  = sum(mass);
+
+x = sum(bsxfun(@times, trj(:, 1:3:end).^2, mass), 2);
+y = sum(bsxfun(@times, trj(:, 2:3:end).^2, mass), 2);
+z = sum(bsxfun(@times, trj(:, 3:3:end).^2, mass), 2);
+rg = sqrt((x + y + z)./totalMass);
+
