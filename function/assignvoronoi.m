c0b6181987d56325b7a068833cf6e9f7738dac55
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2013-01-13 15:53:53 +0900

    update assignvoronoi.m

--- function/assignvoronoi.m
+++ function/assignvoronoi.m
@@ -7,15 +7,22 @@ function [index, dmin] = assignvoronoi(ref, data)
 %# [index, dmin] = assignvoronoi(ref, data)
 %
 %% Description
-% * ref      - generating points of Voronoi cells [n x ndim double]
-% * data     - some trajectory or data set to be assigned [nstep x ndim double]
-% * index    - indices of Voronoi cell. indices correspond to the rows of ref [nstep double]
-% * box      - distance between the data to the nearest generating point [nstep double]
+% This routine assigns Voronoi cell indices to the elements of data
+% by checking the distances between ref and data. For the distance,
+% n-dimensional Euclid distance is used. 
+%
+% * ref   - generating points of Voronoi cells [n x ndim double]
+% * data  - some trajectory or data set to be assigned [nstep x ndim double]
+% * index - indices of Voronoi cell. indices correspond to the rows of ref [nstep double]
+% * box   - distance between the data to the nearest generating point [nstep double]
 %
 %% Example
-%# ref  = randn(100, 1);
-%# data = randn(10000, 1);
+%# ref  = rand(10, 2);
+%# data = rand(10000, 2);
 %# [index, dmin] = assignvoronoi(ref, data);
+%# scatter(data(:, 1), data(:, 2), 5, index, 'filled');
+%# hold on
+%# scatter(ref(:, 1), ref(:, 2), 100, 'ro', 'filled');
 % 
 %% See also
 % assign1dbins, assign2dbins, 
@@ -37,5 +44,4 @@ for istep = 1:nstep
   index(istep) = index_each;
   dmin(istep) = sqrt(dmin_each);
 end
-
 
