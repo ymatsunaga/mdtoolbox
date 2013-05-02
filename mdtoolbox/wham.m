0cb09c2350a971b1780b8f93f8c5de348a3f4441
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2013-05-02 21:34:54 +0900

    update wham

--- mdtoolbox/wham.m
+++ mdtoolbox/wham.m
@@ -1,10 +1,10 @@
-function [f_k, prob_m, center_m, h_km, bias_km, N_m] = wham(edge_m, fhandle_k, data_k, kbt, tolerance)
+function [f_k, log_prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_k, data_k, kbt, tolerance)
 %% wham
-% calculate dimensionless free energies of umbrella windows and the unbiased probabilities on grids by using the weighted histogram analysis method (WHAM)
+% calculate dimensionless free energies of umbrella windows and the unbiased probabilities on grids by using the WHAM
 %
 %% Syntax
-%# [f_k, prob_m, center_m, h_km, bias_km, N_m] = wham(edge_m, fhandle_k, data_k, kbt)
-%# [f_k, prob_m, center_m, h_km, bias_km, N_m] = wham(edge_m, fhandle_k, data_k, kbt, tolerance)
+%# [f_k, prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_k, data_k, kbt)
+%# [f_k, prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_k, data_k, kbt, tolerance)
 %
 %% Description
 %
@@ -12,7 +12,7 @@ function [f_k, prob_m, center_m, h_km, bias_km, N_m] = wham(edge_m, fhandle_k, d
 %               [double M]
 % * fhandle_k - cell of function handles which represent biased potentials
 %               [cell K]
-% * data_m    - cell of trajectories in a space where histograms are counted
+% * data_k    - cell of trajectories in a space where histograms are counted
 %               [cell K]
 % * kbt       - Kb*T in kcal/mol
 %               [double scalar]
@@ -27,14 +27,14 @@ function [f_k, prob_m, center_m, h_km, bias_km, N_m] = wham(edge_m, fhandle_k, d
 %#
 % 
 %% See also
-% mbar
+% ptwham
 %
 %% References
-% [1] S. Kumar, D. Bouzida, R. H. Swendsen, P. A. Kollman, and
-%     J. M. Rosenberg, J. Comput. Chem. 13, 1011 (1992). 
-% [2] B. Roux, Computer Physics Communications 91, 275 (1995).
-% [3] J. D. Chodera, W. C. Swope, J. W. Pitera, C. Seok, and  
+% [1] J. D. Chodera, W. C. Swope, J. W. Pitera, C. Seok, and  
 %     K. A. Dill, J. Chem. Theory Comput. 3, 26 (2007).
+% [2] S. Kumar, D. Bouzida, R. H. Swendsen, P. A. Kollman, and
+%     J. M. Rosenberg, J. Comput. Chem. 13, 1011 (1992). 
+% [3] B. Roux, Computer Physics Communications 91, 275 (1995).
 % [4] J. S. Hub, B. L. de Groot, and D. van der Spoel,
 %     J. Chem. Theory Comput. 6, 3713 (2010). 
 %
@@ -63,10 +63,12 @@ for k = 1:K
   hh = histc(data_k{k}, edge_m);
   h_km(k, :) = hh(1:(end-1))';
 end
-% check the number of histogram counts for each window
-N_m = sum(h_km, 2);
+% the number of histogram counts for each window
+N_k = sum(h_km, 2);
+log_N_k = log(N_k);
+log_N_k(N_k == 0) = -10;
 
-%% calculate bias-energy
+%% calculate bias-factor
 bias_km = zeros(K, M);
 for k = 1:K
   for m = 1:M
@@ -77,7 +79,9 @@ end
 bias_km = bias_km ./ kbt;
 
 %% calculate statistical inefficiency
-
+g_km = ones(K, M);
+g_inv_km = 1./g_km;
+log_g_inv_km = log(g_inv_km);
 
 %% solve the WHAM equations by self-consistent iteration
 f_k = zeros(K, 1);
@@ -86,12 +90,23 @@ check_convergence = inf;
 
 count_iteration = 0;
 while check_convergence > tolerance
-  eq1_inv_km  = exp(bsxfun(@minus, f_k, bias_km));
-  eq1_inv_m   = sum(bsxfun(@times, N_m, eq1_inv_km));
-  prob_m  = sum(h_km)./eq1_inv_m;
-  f_k_new = - logsumexp(-bias_km' + repmat(log(prob_m'), 1, K));
+
+  % 1st equation
+  log_denominator_km = bsxfun(@minus, f_k, bias_km);
+  log_denominator_km = log_denominator_km + log_g_inv_km;
+  log_denominator_km = bsxfun(@plus, log_denominator_km, log_N_k);
+  log_denominator_m  = logsumexp(log_denominator_km);
+
+  log_numerator_m = log(sum(g_inv_km .* h_km));
+
+  log_prob_m = log_numerator_m - log_denominator_m;
+  
+  % 2nd equation
+  f_k_new = - logsumexp(-bias_km' + repmat(log_prob_m', 1, K));
   f_k_new = f_k_new';
   f_k_new = f_k_new - f_k_new(1);
+
+  % check convergence
   check_convergence = max(abs(f_k_new - f_k))./std(f_k_new);
   f_k = f_k_new;
 
@@ -105,11 +120,11 @@ while check_convergence > tolerance
 end
 
 % normalize unbiased probability
-prob_m = prob_m./sum(prob_m);
+%prob_m = prob_m./sum(prob_m);
 
 %% bootstrap?
-f_k_error = 0;
-prob_m_error = 0;
+% f_k_error = 0;
+% prob_m_error = 0;
 
 
 %% logsumexp (input should be array. sums over rows)
