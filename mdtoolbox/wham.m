7d2d950cb41a31144864b4c41416edc81e6ac209
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2013-05-03 18:45:02 +0900

    update wham

--- mdtoolbox/wham.m
+++ mdtoolbox/wham.m
@@ -1,6 +1,6 @@
 function [f_k, log_prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_k, data_k, kbt, tolerance)
 %% wham
-% calculate dimensionless free energies of umbrella windows and the unbiased probabilities on grids by using the WHAM
+% calculate the dimensionless free energies of umbrella windows and the unbiased probabilities on grids by using the WHAM
 %
 %% Syntax
 %# [f_k, prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_k, data_k, kbt)
@@ -20,8 +20,8 @@ function [f_k, log_prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_
 %               [double scalar]
 % * f_k       - dimensionless free energies of umbrella windows
 %               [double K x 1]
-% * prob_m    - unbiased probability of bins (prob(m) = exp(-kbt*U(x_m))/Z(kbt) * dx_m)
-%               [double 1 x M]
+% * log_prob_m  - log of unbiased probability in bins (prob(m) = exp(-kbt*U(x_m))/Z(kbt) * dx_m)
+%                 [double 1 x M]
 % 
 %% Example
 %#
@@ -39,36 +39,62 @@ function [f_k, log_prob_m, center_m, h_km, bias_km, N_k] = wham(edge_m, fhandle_
 %     J. Chem. Theory Comput. 6, 3713 (2010). 
 %
 
-% The names of variables and indicies follow those of Ref [3]. 
-% We assume an array structure whose rows correspond to umbrella
-% windows and columns are bins. 
+% The notation of variables and indicies follows Ref [1]. 
+% Also, we assume an array structure whose 
+% rows(k) correspond to umbrella windows and columns(m) are bins. 
 
 %% preparation
 % K: number of umbrella windows
 K = numel(data_k); 
-assert(K == numel(fhandle_k), 'the numbers of windows in data and fhandle do not match...');
 % M: number of bins
 M = numel(edge_m) - 1;
-% calculate the centers of bins
+% center_m: centers of data bins (m)
 center_m = edge_m + 0.5*(edge_m(2) - edge_m(1));
 center_m(end) = [];
 % tolerance to check convergence of iterations
-if (nargin < 5) | numel(tolerance) == 0
+if (nargin < 5) || numel(tolerance) == 0
   tolerance = 10^(-8);
 end
 
-%% count histogram
+% check consistency of the number of umbrella-windows
+if K ~= numel(fhandle_k)
+  error('# of umbrella windows do not match... data has %d windows. fhandle has %d windows.', numel(data_k), numel(fhandle_k));
+end
+
+%% calculate histogram (h_km)
+% h_km: number of smaples in data-bin (m) from umbrella-window (k)
 h_km = zeros(K, M);
 for k = 1:K
-  hh = histc(data_k{k}, edge_m);
-  h_km(k, :) = hh(1:(end-1))';
+  h_m = histc(data_k{k}, edge_m);
+  h_km(k, :) = h_m(1:(end-1))';
 end
-% the number of histogram counts for each window
+
+%% calculate counts (N_k)
+% N_k: number of samples counted in the histogram from umbrella-window (k)
 N_k = sum(h_km, 2);
-log_N_k = log(N_k);
-log_N_k(N_k == 0) = -10;
+log_N_k = zeros(K, 1);
+log_N_k(N_k > 0) = log(N_k(N_k > 0));
+
+%% calculate statistical inefficiency (g_km)
+% g_km: statistical efficiency of indicator function for umbrella-window (k) in data-bin (m)
+g_km = ones(K, M);
+
+%% calculate effective histogram (heff_m)
+% heff_m: effective number of independent samples in data-bin (m)
+heff_m = sum(h_km./g_km);
+log_heff_m = zeros(1, M);
+log_heff_m(:) = double(log(eps('single')));
+log_heff_m(heff_m > 0) = log(heff_m(heff_m > 0));
+
+%% calculate effective counts (Neff_km)
+% Neff_km: effective number of independent samples for umbrella window (k) in data-bin (m)
+Neff_km = bsxfun(@rdivide, N_k, g_km);
+log_Neff_km = zeros(K, M);
+log_Neff_km(:) = double(log(eps('single')));
+log_Neff_km(Neff_km > 0) = log(Neff_km(Neff_km > 0));
 
 %% calculate bias-factor
+% bias_km: bias-factor for umbrella window (k) in data-bin (m)
 bias_km = zeros(K, M);
 for k = 1:K
   for m = 1:M
@@ -78,31 +104,25 @@ end
 % convert to dimensionless energies
 bias_km = bias_km ./ kbt;
 
-%% calculate statistical inefficiency
-g_km = ones(K, M);
-g_inv_km = 1./g_km;
-log_g_inv_km = log(g_inv_km);
-
 %% solve the WHAM equations by self-consistent iteration
 f_k = zeros(K, 1);
-prob_m = zeros(1, M);
+log_prob_m = zeros(1, M);
 check_convergence = inf;
 
 count_iteration = 0;
 while check_convergence > tolerance
 
   % 1st equation
-  log_denominator_km = bsxfun(@minus, f_k, bias_km);
-  log_denominator_km = log_denominator_km + log_g_inv_km;
-  log_denominator_km = bsxfun(@plus, log_denominator_km, log_N_k);
+  log_denominator_km = bsxfun(@plus, f_k, -bias_km);
+  log_denominator_km = log_denominator_km + log_Neff_km;
   log_denominator_m  = logsumexp(log_denominator_km);
 
-  log_numerator_m = log(sum(g_inv_km .* h_km));
+  log_numerator_m = log_heff_m;
 
   log_prob_m = log_numerator_m - log_denominator_m;
   
   % 2nd equation
-  f_k_new = - logsumexp(-bias_km' + repmat(log_prob_m', 1, K));
+  f_k_new = - logsumexp(bsxfun(@plus, log_prob_m', -bias_km'));
   f_k_new = f_k_new';
   f_k_new = f_k_new - f_k_new(1);
 
@@ -119,9 +139,6 @@ while check_convergence > tolerance
   end
 end
 
-% normalize unbiased probability
-%prob_m = prob_m./sum(prob_m);
-
 %% bootstrap?
 % f_k_error = 0;
 % prob_m_error = 0;
@@ -129,8 +146,7 @@ end
 
 %% logsumexp (input should be array. sums over rows)
 function s = logsumexp(x)
-[K, M] = size(x);
 max_x = max(x);
-exp_x= exp(x - repmat(max_x, K, 1));
+exp_x = exp(bsxfun(@minus, x, max_x));
 s = log(sum(exp_x)) + max_x;
 
