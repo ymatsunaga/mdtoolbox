a3d6648689433bf8d5bbadd56e0f9f40aa806886
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2013-06-13 19:24:02 +0900

    add principal component analysis (calcpca.m)

---
+++ mdtoolbox/calcpca.m
@@ -0,0 +1,36 @@
+function [p, pmode, variances]  = calcpca(trj)
+%% calcpca
+% calc principal components
+%
+%% Syntax
+%# q = calcbond(trj);
+%
+%% Description
+%
+% * trj    - trajectory [nstep x 3natom]
+%
+%% Example
+%#
+%
+%% See alo
+% calcangle, calcdihedral
+% 
+
+%% setup
+nstep = size(trj, 1);
+
+%% covariance matrix
+trj = bsxfun(@minus, trj, mean(trj));
+covmat = (trj'*trj)./(nstep-1); %unbiased
+%covmat = (trj'*trj)./nstep;     %biased
+
+%% diagonalize
+[eigenVector, eigenValue] = eig(covmat);
+eigenValue = diag(eigenValue);
+[variances, index] = sort(eigenValue, 1, 'descend');
+pmode = eigenVector(:, index);
+
+%% projection
+p = trj * pmode;
+
+
