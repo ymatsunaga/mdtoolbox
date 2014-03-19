068e81f0aa8afc6f4736c0a63dba0ad9d4d4c85b
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2014-03-19 12:20:36 +0900

    update make

--- make.m
+++ make.m
@@ -1,21 +1,17 @@
-function make(target, opts)
+function make(target)
 
 %% mex files list
 mex_list{1} = 'superimpose.c'; % least-squares fitting routine
 mex_list{2} = 'mbar_log_wi_jn.c'; % mbar auxiliary function;
 
 %% setup
-if (nargin < 1) || isempty(target)
+if (~exist('target', 'var')) | isempty(target)
   target = 'all';
-end
-
-if (nargin < 2)
-  opts = '';
 end
 
 %% get fullpath to mdtoolbox
 fullpath = mfilename('fullpath');
-[path_mdtoolbox, filename] = fileparts(fullpath);
+path_mdtoolbox = fileparts(fullpath);
 
 %% compile mex files
 current_dir = pwd;
@@ -23,26 +19,46 @@ source_dir = [path_mdtoolbox filesep 'mdtoolbox'];
 
 cd(source_dir);
 
-if target == 'all'
-  opts = '-v'
+if strcmp(target, 'all')
+  opts = '';
   compile_mex(mex_list, opts);
-elseif target == 'debug'
+
+elseif strcmp(target, 'verbose')
+  opts = '-v';
+  compile_mex(mex_list, opts);
+
+elseif strcmp(target, 'debug')
   option = '-g -DDEBUG';
   compile_mex(mex_list, opts);
-elseif target == 'openmp'
+
+elseif strcmp(target, 'openmp')
   option = 'LDFLAGS="-fopenmp \$LDFLAGS" CFLAGS="-fopenmp \$CFLAGS"';
   compile_mex(mex_list, opts);
-elseif target == 'clean'
+
+elseif strcmp(target, 'clean')
+  delete_mex(mex_list);
 
 else
   error(sprintf('cannot find target: %s', target));
+
 end
 
 cd(current_dir);
 
 function compile_mex(mex_list, opts)
 for i = 1:numel(mex_list)
-  fprintf('Compiling MEX functinon %s ...\n', mex_list{i});
+  fprintf('Compiling MEX code: %s ...\n', mex_list{i});
   eval(sprintf('mex %s %s', opts, mex_list{i}));
+  fprintf('\n');
+end
+
+function delete_mex(mex_list)
+for i = 1:numel(mex_list)
+  [~, filename] = fileparts(mex_list{i});
+  mex_exe = [filename '.' mexext];
+  fprintf('Deleting MEX exe: %s ...\n', mex_exe);
+  if (exist(mex_exe, 'file'))
+    delete(mex_exe);
+  end
 end
 
