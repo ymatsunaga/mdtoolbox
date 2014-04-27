83c98448102f9ca6c89d5597d02208c6cd68bc52
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2014-04-27 17:07:56 +0900

    add covariance and cross-correlation routines for atomic fluctuations

---
+++ mdtoolbox/calccorr.m
@@ -0,0 +1,53 @@
+function corr_atom = calccorr(trj)
+%% calccovar
+% calculate cross-correlation matrix from input trajectory
+%
+%% Syntax
+%# covar = calccovar(trj);
+%# [covar, covar_atom] = calccovar(trj);
+%# [~, covar_atom] = calccovar(trj);
+%
+%% Description
+% This routine calculates the Pearson correlation coefficients 
+% between atomi and atom j, and make a cross-correlation matrix
+% from input trajectory
+% Prior to the calculation of the covariance,
+% superimposing is automatically applied to eliminate external
+% translations and rotations. In the current implementation,
+% mass-weight calculation is not supported. Just assuming uniform
+% mass for all atoms.
+%
+% * trj       - trajectory of coordinates [nstep x 3natom]
+% * corr_atom - cross correlation matrix of atomic fluctuations. 
+%       covar_atom(i,j) = <dx_i dx_j + dy_i dy_j + dz_i dz_j>
+%       / sqrt(<dx_i^2 + dy_i^2 + dz_i^2><dx_j^2 + dy_j^2 + dz_j^2>)
+%                 [double natom x natom]
+%
+%% Example
+%# trj = readdcd('ak.dcd');
+%# corr_atom = calccorr(trj);
+%# imagesc(corr_atom, [-1, 1]);
+%# axis xy; axis square; colorbar;
+%# xlabel('residue', 'fontsize', 25);
+%# ylabel('residue', 'fontsize', 25);
+%# exportas('corr');
+%
+%% See also
+% calccovar
+% 
+
+%% setup
+nstep = size(trj, 1);
+natom = size(trj, 2)/3;
+
+%% calc covariance matrix
+covar_atom = calccovar(trj);
+
+%% normalize covariance matrix
+corr_atom = zeros(natom, natom);
+for i = 1:natom
+  for j = 1:natom
+    corr_atom(i,j) = covar_atom(i,j)./sqrt(covar_atom(i,i)*covar_atom(j,j));
+  end
+end
+
--- mdtoolbox/calccovar.m
+++ mdtoolbox/calccovar.m
@@ -1,25 +1,34 @@
-function [covar, covar_atom] = calccovar(trj)
+function covar_atom = calccovar(trj)
 %% calccovar
-% calculate covariance-matrix from trajectory
+% calculate covariance-matrix from input trajectory
 %
 %% Syntax
-%# covar = calccovar(trj);
+%# covar_atom = calccovar(trj);
 %
 %% Description
+% This routine calculates a covariance matrix from input
+% trajectory. Prior to the calculation of the covariance,
+% superimposing is automatically applied to eliminate external
+% translations and rotations. In the current implementation,
+% mass-weight calculation is not supported. Just assuming uniform
+% mass for all atoms.
 %
 % * trj        - trajectory of coordinates [nstep x 3natom]
-% * covar      - covariance matrix of anisotropic fluctuations. 
-%                 covar(i,j) = <q_i q_j>
-%                 [double natom3 x natom3]
-% * covar_atom - covariance matrix of isotropic fluctuations. 
-%                 covar_atom(i,j) = <x_i x_j + y_i y_j + z_i z_j>
+% * covar_atom - covariance matrix of atomic fluctuations. 
+%                 covar_atom(i,j) = <dx_i dx_j + dy_i dy_j + dz_i dz_j>
 %                 [double natom x natom]
 %
 %% Example
-%#
+%# trj = readdcd('ak.dcd');
+%# covar_atom = calccovar(trj);
+%# imagesc(covar_atom);
+%# axis xy; axis square; colorbar;
+%# xlabel('residue', 'fontsize', 25);
+%# ylabel('residue', 'fontsize', 25);
+%# exportas('covar');
 %
-%% See alo
-% calcpca
+%% See also
+% calccorr
 % 
 
 %% setup
@@ -35,14 +44,12 @@ trj = bsxfun(@minus, trj, mean(trj));
 covar = (trj'*trj)./(nstep-1);  % unbiased estimates of covariances
 %covar = (trj'*trj)./nstep;     % biased estimates of covariances
 
-if nargout >= 2
-  covar_atom = zeros(natom,natom);
-  for i = 1:natom
-    for j = 1:natom
-      covar_atom(i,j) = covar(3*(i-1)+1, 3*(j-1)+1) ...
-                      + covar(3*(i-1)+2, 3*(j-1)+2) ...
-                      + covar(3*(i-1)+3, 3*(j-1)+3);
-    end
+covar_atom = zeros(natom, natom);
+for i = 1:natom
+  for j = 1:natom
+    covar_atom(i,j) = covar(3*(i-1)+1, 3*(j-1)+1) ...
+                    + covar(3*(i-1)+2, 3*(j-1)+2) ...
+                    + covar(3*(i-1)+3, 3*(j-1)+3);
   end
 end
 
