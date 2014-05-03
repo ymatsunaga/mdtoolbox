2cab0938582a502026b5887c662fc783eddb8547
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2014-05-04 07:31:08 +0900

    update calcpmf2d.m

--- mdtoolbox/calcpmf2d.m
+++ mdtoolbox/calcpmf2d.m
@@ -1,14 +1,15 @@
-function [z,xi,yi] = calcpmf2d(x,nbin)
+function [z, xi, yi] = calcpmf2d(x, nbin)
 %% calcpmf2d
 % calculate 2D potential of mean force from the scattered 2D-data (using kernel density estimator)
 %
-% function [z,xi,yi] = fene2d(x,nbin)
+%% Syntax
+% [pmf, xi, yi] = calcpmf2d(data)
+% [pmf, xi, yi] = calcpmf2d(data, nbin)
 %
-% %AKの粗視化モデルの主成分データを読み込む
+%% Example
 % load p.mat;
-% %ヒストグラム計算に用いるbinの数
-% nbin = 100;
-% [z,xi,yi] = fene2d(p(:,[1 2]),nbin);
+% nbin = 256;
+% [z, xi, yi] = calcpmf2d(p(:,[1 2]), nbin);
 % imagesc(xi,yi,z); axis xy;
 % xlabel('PC1','FontSize',45); ylabel('PC2','FontSize',45); colorbar; plot_format
 %
@@ -17,7 +18,7 @@ xd = x(:,1);
 yd = x(:,2);
 
 if ~exist('nbin', 'var') || isempty(nbin)
-  nbin = 100;
+  nbin = 256;
 end
 
 xi = linspace(min(xd),max(xd),nbin);
