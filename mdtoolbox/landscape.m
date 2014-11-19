6143c50d1b93e9ad599f098b3a405c0dc7412cf8
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2014-11-19 11:51:44 +0900

    add a drawing routine for 2-D free energy landscape (landscape.m)

---
+++ mdtoolbox/landscape.m
@@ -0,0 +1,41 @@
+function landscape(xi, yi, data, level)
+%% landscape
+% draws 2-dimensional free energy surface and its contour lines
+%
+%% Syntax
+%# landscape(xi, yi, data)
+%# landscape(xi, yi, data, level, level_max)
+%
+%% Description
+%
+% * xi    - regularly spaced grids in x-axis [n x 1 or 1 x n double vector]
+% * yi    - regularly spaced grids in y-axis [m x 1 or 1 x m double vector]
+% * data  - free energy data [m x n double array]
+% * level - contour levels [doulbe vector]
+%
+%% Example
+%#
+%
+
+%% preparation
+data = data - min(data(:));
+if ~exist('level', 'var') || isempty(level)
+  level = 0:0.25:max(data(:));
+end
+
+level_max = max(level);
+
+%% plot
+data2 = data;
+data2(data2 > level_max) = NaN;
+pcolor(xi, yi, data2);
+shading flat;
+colorbar;
+axis([min(xi) max(xi) min(yi) max(yi)]);
+axis xy;
+formatplot2;
+
+hold on;
+contour(xi, yi, data2, level, 'linecolor', 'black');
+hold off;
+
