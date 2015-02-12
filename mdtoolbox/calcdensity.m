ae732d4f7eef5bc3a7fe10199de7da26cd35fb14
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-02-12 18:00:20 +0900

    now 3D density is computed using kernel density estimate

--- mdtoolbox/calcdensity.m
+++ mdtoolbox/calcdensity.m
@@ -1,37 +1,59 @@
-function map = calcdensity(trj, edges)
+function density = calcdensity(trj, xi, yi, zi, bandwidth, weight)
 %% calcdensity
-% calculate 3-D g(r) of (solvent) atoms with 3-D grids from trajectory data
+% compute 3-dimensional density of atoms from trajectory data
 %
 %% Syntax
-%# map = calcdensity(trj);
-%# map = calcdensity(trj, edges);
+%# density = calcdensity(trj);
+%# density = calcdensity(trj, edges);
 %
+%% Description
+%
+% * trj       - trajectory data [nstep x natom3 double]
+% * xi        - equally spaced grid in the x-axis on which
+%               the density is estimated [1 x nx double]
+% * yi        - equally spaced grid in the y-axis on which
+%               the density is estimated [1 x ny double]
+% * zi        - equally spaced grid in the z-axis on which
+%               the density is estimated [1 x ny double]
+% * bandwidth - bandwidth of the Gaussian kernel function
+%               [1 x 3 double]
+% * weight    - weights of trajectory snapshots. 
+%               By default, uniform weight is used. 
+%               [nstep x 1 double]
+% * density   - volumetric data of 3-dimensional density
+%               [nx x ny x nz double]
+%               
 %% Example:
-% [trj, box] = readfloattrj(natom, 'md.trj', 1:natom, true);
-% box = box(end, :);
-% nbins = 180;
-% edges{1} = linspace(0, box(1), nbins+1);
-% edges{2} = linspace(0, box(2), nbins+1);
-% edges{3} = linspace(0, box(3), nbins+1);
-% map = density(trj(:,index3_o), edges);
-% writexplormap('holo_o.xplor', map, box);
+% [trj, box] = readdcd('run.dcd');
+% xi = linspace(-10, 10, 100);
+% yi = linspace(-10, 10, 100);
+% zi = linspace(-10, 10, 100);
+% density = density(trj, xi, yi, zi, [1.0 1.0 1.0]);
+% writedx('density.dx', density, xi, yi, zi);
 %
 
-nstep = size(trj, 1);
-natom = size(trj, 2)./3;
-nbins = length(edges{1})-1;
-bulk_density = (natom./(nbins.^3));
+%% preparation
+nstep  = size(trj, 1);
+natom3 = size(trj, 2);
+natom  = natom3./3;
+%bulk_density = (natom./(nbins.^3));
 
-map = zeros(nbins, nbins, nbins);
-crd = zeros(1, natom*3);
-xyz = zeros(natom, 3);
-for istep = 1:nstep
-  crd = trj(istep, :);
-  xyz = reshape(crd, 3, natom)';
-  [map2, edges2, mid, loc] = histcn(xyz, edges{1}, edges{2}, edges{3});
-  %map = map + map2;
-  map = map + map2(1:nbins, 1:nbins, 1:nbins);
+nx = numel(xi);
+ny = numel(yi);
+nz = numel(zi);
+density = zeros(nx, ny, nz);
+
+if ~exist('weight', 'var') || isempty(weight)
+  weight = ones(nstep, 1);
 end
-map = map./bulk_density;
-map = map./nstep;
+weight = weight./sum(weight);
+
+%% compute density
+for istep = 1:nstep
+  data = reshape(trj(istep, :), 3, natom)';
+  f = ksdensity3d(data, xi, yi, zi, bandwidth);
+  density = density + f*natom*weight(istep);
+end
+%density = density./bulk_density;
+%density = density./nstep;
 
