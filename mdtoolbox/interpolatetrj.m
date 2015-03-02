8b3e149325cfebd4af704362211c976a94e7cb63
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-03-02 10:08:01 +0900

    update comments

--- mdtoolbox/interpolatetrj.m
+++ mdtoolbox/interpolatetrj.m
@@ -1,24 +1,22 @@
 function trj_interpolated = interpolatetrj(trj, nsnapshot)
 %% interpolatetrj
-% interpolate input trajectory by the natural cubic splines
+% interpolate input trajectory by cubic spline
 %
 %% Syntax
 %# trj_interpolated = interpolatetrj(trj, nsnapshot);
 %
 %% Description
 % This routine interpolates infrequently saved trajectory
-% by using the cubic splines.
-% This interpolation is just for convenience in visual inspection, 
-% and should be carefull for the interpreation of the
-% interplated trajectory.
+% by using natural cubic splines.
+% This interpolation is just for nice visual inspection. 
+% thus should not be treated for scientific analysis. 
 %
-% * trj       - coordinates of atoms [nstep x natom3]
-% * nsnapshot - the number of snapshots created by the interpolation [scalar integer]
-% * trj       - interpolated trajectory [nsnapshot x natom3]
+% * trj              - trajectory data [nstep x natom3]
+% * nsnapshot        - the number of snapshots created by the interpolation [scalar integer]
+% * trj_interpolated - interpolated trajectory data [nsnapshot x natom3]
 %
 %% Example
 %# trj = readnetcdf('ala.nc');
-%# trj = trj(1:10, :);
 %# trj_interpolated = interpolatetrj(trj, 1000);
 %
 %% See alo
@@ -32,7 +30,6 @@ end
 nstep = size(trj, 1);
 index_time = 1:nstep;
 index_time_interpolation = linspace(1, nstep, nsnapshot);
-
 
 %% natural cubic spline interpolation
 cs = spline(index_time, trj');
