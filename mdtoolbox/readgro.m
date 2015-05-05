bcc559dcd9b977564624d39eff0b56777cea3419
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-05-05 17:18:10 +0900

    preliminary version of gromacs gro I/O routines

---
+++ mdtoolbox/readgro.m
@@ -0,0 +1,86 @@
+function [gro, crd, vel] = readgro(filename, index)
+%% readgro
+% read gromacs gro (Gromos87 format) file
+%
+%% Syntax
+%# [gro, crd] = readgro(filename);
+%# [gro, crd, vel] = readgro(filename);
+%
+%% Description
+% This routine reads gromacs gro (Gromos87 format) file.
+% Infomration of the gro file is stored in a structure variable.
+% Coordinates, and velocities can be separately obtained
+% if the user specifies additional output arguments.
+%
+% * filename  - input filename of gromacs gro file [char]
+% * gro       - structure data
+%     residue_number: residue number [natom x 1 integer]
+%       residue_name: residue name [natom x 5 char]
+%          atom_name: atom name [natom x 5 char]
+%        atom_number: atom number [natom x 1 integer]
+%           position: atom XYZ coordinates [natom x 3 double]
+%           velocity: atom XYZ velocities [natom x 3 double]
+% * crd       - coordinates [1 x natom3 double]
+%               if the gro file contains multiple structures
+%               coordinates [nstep x natom3 double]
+% * vel       - velocities [1 x natom3 double]
+%               if the gro file contains multiple structures
+%               velocities [nstep x natom3 double]
+% * box       - box size [1 x 3 double]
+%
+%% Example
+%# [gro, crd] = readgro('out.gro');
+%
+%% See alo
+% writegro
+%
+%% References
+% http://manual.gromacs.org/current/online/gro.html
+%
+%% TODO
+% time step in title
+% multiple steps
+%
+
+%% open file
+fid = fopen(filename, 'r');
+assert(fid > 0, 'Could not open file.');
+cleaner = onCleanup(@() fclose(fid));
+
+%% parse
+title = fgetl(fid);
+str = fgetl(fid);
+natom = sscanf(str, '%d', 1);
+
+%% allocate variables
+gro.residue_number = zeros(natom, 1);
+gro.residue_name   = repmat('12345', natom, 1);
+gro.atom_name      = repmat('12345', natom, 1);
+gro.atom_number    = zeros(natom, 1);
+gro.position       = zeros(natom, 3);
+gro.velocity       = zeros(natom, 3);
+gro.box            = zeros(1, 3);
+
+%% read file
+for iatom = 1:natom
+  line = fgetl(fid);
+
+  gro.residue_number(iatom)  = str2num(line(1:5));
+  gro.residue_name(iatom, :) = line(6:10);
+  gro.atom_name(iatom, :)    = line(11:15);
+  gro.atom_number(iatom)     = str2num(line(16:20));
+  gro.position(iatom, 1)     = str2num(line(21:28));
+  gro.position(iatom, 2)     = str2num(line(29:36));
+  gro.position(iatom, 3)     = str2num(line(37:44));
+  
+end
+
+line = fgetl(fid);
+gro.box(1, :) = sscanf(line, '%f', 3);
+
+if nargout >= 2
+  crd = zeros(1, 3*natom);
+  tmp = gro.position';
+  crd(1, :) = tmp(:)';
+end
+
---
+++ mdtoolbox/writegro.m
@@ -0,0 +1,85 @@
+function writegro(filename, gro, trj)
+%% writegro
+% write gromacs gro (Gromos87 format) file
+%
+%% Syntax
+%# writegro(filename, gro);
+%# writegro(filename, gro, crd);
+%
+%% Description
+% This routine writes gromacs gro (Gromos87 format) file.
+% Infomration of the gro file is stored in a structure variable.
+% If coordinates are given, coordinates of output gro file 
+% are repaced with the given coordinates
+%
+% * filename  - output filename of gromacs gro file [char]
+% * gro       - structure data
+%     residue_number: residue number [natom x 1 integer]
+%       residue_name: residue name [natom x 5 char]
+%          atom_name: atom name [natom x 5 char]
+%        atom_number: atom number [natom x 1 integer]
+%           position: atom XYZ coordinates [natom x 3 double]
+%           velocity: atom XYZ velocities [natom x 3 double]
+% * crd,trj   - coordinates [1 x natom3 double]
+%               if the gro file contains multiple structures
+%               coordinates [nstep x natom3 double]
+% * vel       - velocities [1 x natom3 double]
+%               if the gro file contains multiple structures
+%               velocities [nstep x natom3 double]
+% * box       - box size [1 x 3 double]
+%
+%% Example
+%# gro = readpsf('jac.gro');
+%# gro.xyz(:, 1) = gro.xyz(:, 1) + 2.5; %translate in x-axis by 2.5 Angstrom
+%# writegro('jac_x.gro', gro);
+%
+%% See also
+% readgro
+% 
+%% References
+% http://manual.gromacs.org/current/online/gro.html
+%
+%% TODO
+% time step in title
+% multiple steps
+%
+
+%% check existing file
+if exist(filename, 'file')
+  filename_old = sprintf('%s.old', filename);
+  display(sprintf('existing file %s is moved to %s', filename, filename_old));
+  movefile(filename, filename_old);
+end
+
+%% preparation
+natom = size(gro.atom_number, 1);
+
+if ~exist('trj', 'var') || isempty(trj)
+  trj = gro.position';
+  trj = trj(:)';
+end
+nstep = size(trj, 1);
+  
+%% open file
+fid = fopen(filename, 'w');
+assert(fid > 0, 'Could not open file.');
+cleaner = onCleanup(@() fclose(fid));
+
+%% write file
+fprintf(fid, 'REMARKS FILENAME=%s CREATED BY MATLAB\n', filename);
+fprintf(fid, '%5d\n', natom);
+
+istep = 1;
+for iatom = 1:natom
+  fprintf(fid, '%5d', gro.residue_number(iatom));
+  fprintf(fid, '%-5s', gro.residue_name(iatom, :));
+  fprintf(fid, '%5s', gro.atom_name(iatom, :));
+  fprintf(fid, '%5d', gro.atom_number(iatom, :));
+  fprintf(fid, '%8.3f', trj(istep, 3*(iatom-1)+1));
+  fprintf(fid, '%8.3f', trj(istep, 3*(iatom-1)+2));
+  fprintf(fid, '%8.3f', trj(istep, 3*(iatom-1)+3));
+  fprintf(fid, '\n');
+end
+  
+fprintf(fid, '%10.5f%10.5f%10.5f\n', gro.box(1), gro.box(2), gro.box(3));
+
