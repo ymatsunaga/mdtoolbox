f7b76ff947b49e1168af2bde97b3ee3a2f1c4f19
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-06-11 13:31:22 +0900

    minor update

--- mdtoolbox/readdcd.m
+++ mdtoolbox/readdcd.m
@@ -28,7 +28,7 @@ function [trj, box, header] = readdcd(filename, index)
 %% See also
 % writedcd
 %
-%% References
+%% References for dcd format
 % MolFile Plugin http://www.ks.uiuc.edu/Research/vmd/plugins/molfile/dcdplugin.html
 % CafeMol Manual http://www.cafemol.org/doc.php
 % EGO_VIII Manual http://www.lrz.de/~heller/ego/manual/node93.html
