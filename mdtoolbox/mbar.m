ca11a49f01767ea22fe5b639c905ceccbb5f4a57
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-06-15 17:54:07 +0900

    mbar routines support block average

--- mdtoolbox/mbar.m
+++ mdtoolbox/mbar.m
@@ -1,17 +1,19 @@
-function f_k = mbar(u_kl, tolerance)
+function f_k = mbar(u_kl, nblock)
 %% mbar
 % calculate the free energy differences of umbrella-windowed systems by using the Multistate Bennet Acceptance Ratio Method (MBAR)
 %
 %% Syntax
 %# f_k = mbar(u_kl)
-%# f_k = mbar(u_kl, tolerance)
+%# f_k = mbar(u_kl, nblock)
 %
 %% Description
 %
 % * u_kl      - reduced potential energy of umbrella simulation k evaluated at umbrella l
 %               [cell numbrella x numbrella]
+% * nblock    - the number of blocks in each of which free energies are evaluated, default is 1
+%               [integer scalar]
 % * f_k       - (dimensionless) free energies of umbrella-windows
-%               [double numbrella x 1]
+%               [double numbrella x nblock]
 % 
 %% Example
 %#
@@ -29,7 +31,11 @@ function f_k = mbar(u_kl, tolerance)
 % k, l, i, j run over the umbrella-windows.
 % n runs over the snapshots.
 
-%% preparation
+%%%%%%%% main
+if ~exist('nblock', 'var') || isempty(nblock)
+  nblock = 1;
+end
+
 % K: number of umbrella windows
 [K, L] = size(u_kl);
 assert(K == L, 'the numbers of rows and columns of u_kl should be same...');
@@ -39,21 +45,48 @@ N_k = zeros(K, 1);
 for k = 1:K
   N_k(k) = size(u_kl{k, 1}, 1);
 end
-N_max = max(N_k);
+
+% f_k: evaluate free energies in each block
+f_k = {};
+index_k = {};
+for iblock = 1:nblock
+  for k = 1:K
+    interface = round(linspace(0, N_k(k), nblock+1));
+    index_k{k} = (interface(iblock)+1):interface(iblock+1);
+  end
+  if nblock > 1
+    f_k{iblock} = kernelfunction(u_kl, index_k, iblock);
+  else
+    f_k{iblock} = kernelfunction(u_kl, index_k);
+  end
+end
+
+f_k = cell2mat(f_k);
+
+%%%%%%%% kernel function
+function f_k = kernelfunction(u_kl, index_k, iblock);
 
 % tolerance to check convergence of iterations
-if ~exist('tolerance', 'var') || isempty(tolerance)
-  tolerance = 10^(-8);
+tolerance = 10^(-8);
+
+% K: number of umbrella windows
+[K, L] = size(u_kl);
+
+% N_k: number of data in k-th umbrella window
+N_k = zeros(K, 1);
+for k = 1:K
+  N_k(k) = size(u_kl{k, 1}(index_k{k}), 1);
 end
+N_max = max(N_k);
 
 % conversion from cell (u_kl) to array (u_kln)
 u_kln = zeros(K, K, N_max);
 for k = 1:K
   for l = 1:K
-    u_kln(k, l, 1:N_k(k)) = u_kl{k, l};
+    u_kln(k, l, 1:N_k(k)) = u_kl{k, l}(index_k{k});
   end
 end
-clear u_kl;
+%clear u_kl;
 
 %% solve the MBAR equation by self-consistent iteration
 f_k = zeros(K, 1);
@@ -75,7 +108,11 @@ for count_iteration = 1:5
   check_convergence = max(abs(f_k_new - f_k))./std(f_k_new);
   f_k = f_k_new;
 
-  fprintf('%dth iteration  delta = %e  tolerance = %e\n', count_iteration, check_convergence, tolerance);
+  if exist('iblock', 'var')
+    fprintf('[block %d] %dth iteration  delta = %e  tolerance = %e\n', iblock, count_iteration, check_convergence, tolerance);
+  else
+    fprintf('%dth iteration  delta = %e  tolerance = %e\n', count_iteration, check_convergence, tolerance);
+  end
   fprintf('free energies = ');
   fprintf('%f ', f_k);
   fprintf('\n');
@@ -129,7 +166,11 @@ while check_convergence > tolerance
   f_k = f_k_new;
 
   count_iteration = count_iteration + 1;
-  fprintf('%dth iteration  delta = %e  tolerance = %e\n', count_iteration, check_convergence, tolerance);
+  if exist('iblock', 'var')
+    fprintf('[block %d] %dth iteration  delta = %e  tolerance = %e\n', iblock, count_iteration, check_convergence, tolerance);
+  else
+    fprintf('%dth iteration  delta = %e  tolerance = %e\n', count_iteration, check_convergence, tolerance);
+  end
   fprintf('free energies = ');
   fprintf('%f ', f_k);
   fprintf('\n');
@@ -148,7 +189,7 @@ fprintf('\n');
 fprintf('\n');
 
 
-%% logsumexp (input should be a vector)
+%%%%%%%% logsumexp (input should be a vector)
 function s = logsumexp(x)
 max_x = max(x);
 exp_x = exp(x - max_x);
--- mdtoolbox/mbarpmf.m
+++ mdtoolbox/mbarpmf.m
@@ -12,15 +12,16 @@ function [pmf_i, w_k] = mbarpmf(u_kl, bin_k, f_k, u_k)
 %
 % * u_kl      - reduced potential energy of umbrella simulation k evaluated at umbrella l
 %               [cell numbrella x numbrella]
-% * bin_k     - binned trajectories of umbrella simulation k
+% * bin_k     - binned trajectories of umbrella simulation k. 
 %               [cell numbrella x 1]
 % * f_k       - reduced free energies of umbrella k
-%               [double numbrella x 1]
+%               the number of blocks for block averaing is taken from the colum size of f_k
+%               [double numbrella x nblock]
 % * u_k       - reduced potential energy umbrella simulation k under the condition where PMF is evaluated 
 %               [cell numbrella x 1]
 %               If omitted, u_k = 0 is assumed
 % * pmf       - potential mean force of each bin
-%               [double ngrid x 1]
+%               [double ngrid x nblock]
 % * w_k       - normalized weights for each snapshot used for the calculation of pmf
 %               [cell numbrella x 1]
 % 
@@ -40,7 +41,13 @@ function [pmf_i, w_k] = mbarpmf(u_kl, bin_k, f_k, u_k)
 % We assume array structures whose rows correspond to umbrella
 % windows and columns are bins. 
 
-%% preparation
+%%%%%%%% main
+nblock = size(f_k, 2);
+
+if ~exist('u_k', 'var')
+  u_k = [];
+end
+
 % K: number of umbrella windows
 [K, L] = size(u_kl); 
 assert(K == L, 'the numbers of rows and columns of u_kl should be same...');
@@ -50,13 +57,59 @@ N_k = zeros(K, 1);
 for k = 1:K
   N_k(k) = size(u_kl{k, 1}, 1);
 end
+
+% pmf_i: evaluate potential of mean force for each block
+if isempty(bin_k)
+  pmf_i = [];
+else
+  pmf_i = {};
+  index_k = {};
+  for iblock = 1:nblock
+    for k = 1:K
+      interface = round(linspace(0, N_k(k), nblock+1));
+      index_k{k} = (interface(iblock)+1):interface(iblock+1);
+    end
+    pmf_i{iblock} = kernelfunction(u_kl, bin_k, f_k, u_k, index_k);
+  end
+  pmf_i = cell2mat(pmf_i);
+  
+  if nblock > 1
+    [~, index_min] = min(pmf_i(:, 1));
+    pmf_i = pmf_i - pmf_i(index_min, 1);
+    pmf_i = [mean(pmf_i, 2) std(pmf_i, [], 2)];
+  else
+    pmf_i = pmf_i - min(pmf_i);
+  end
+end
+
+if nargout > 1
+  if nblock > 1
+    f_k = mean(f_k, 2);
+  end
+  for k = 1:K
+    index_k{k} = 1:N_k(k);
+  end
+  [~, w_k] = kernelfunction(u_kl, [], f_k, u_k, index_k);
+end
+
+%%%%%%%% kernel function
+function [pmf_i, w_k] = kernelfunction(u_kl, bin_k, f_k, u_k, index_k);
+
+% K: number of umbrella windows
+[K, L] = size(u_kl); 
+
+% N_k: number of data in k-th umbrella window
+N_k = zeros(K, 1);
+for k = 1:K
+  N_k(k) = size(u_kl{k, 1}(index_k{k}), 1);
+end
 N_max = max(N_k);
 
 % conversion from cell (u_kl) to array (u_kln)
 u_kln = zeros(K, K, N_max);
 for k = 1:K
   for l = 1:K
-    u_kln(k, l, 1:N_k(k)) = u_kl{k, l};
+    u_kln(k, l, 1:N_k(k)) = u_kl{k, l}(index_k{k});
   end
 end
 clear u_kl;
@@ -65,7 +118,7 @@ clear u_kl;
 u_kn = zeros(K, N_max);
 if exist('u_k', 'var') && ~isempty(u_k);
   for k = 1:K
-    u_kn(k, 1:N_k(k)) = u_k{k};
+    u_kn(k, 1:N_k(k)) = u_k{k}(index_k{k});
   end
   clear u_k;
 end
@@ -76,7 +129,7 @@ if isempty(bin_k)
 else
   bin_kn = zeros(K, N_max);
   for k = 1:K
-    bin_kn(k, 1:N_k(k)) = bin_k{k};
+    bin_kn(k, 1:N_k(k)) = bin_k{k}(index_k{k});
   end
   clear bin_k;
 end
