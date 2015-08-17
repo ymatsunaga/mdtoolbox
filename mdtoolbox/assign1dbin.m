b8194f1711c15ebaf55b7a30ae30407a17b5861e
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-08-17 17:47:25 +0900

    bugfix

--- mdtoolbox/assign1dbin.m
+++ mdtoolbox/assign1dbin.m
@@ -1,57 +1,49 @@
-function [index, center, edge] = assign1dbins(data, edge)
-%% assign1dbins
-% assign bin indices to the given data by binning their values
+function [index, histogram] = assign1dbin(data, edge)
+%% assign1dbin
+% assign bin index to the given 1-D data by binning their values
 %
 %% Syntax
-%# index = assign1dbins(data, edge);
-%# [index, center] = assign1dbins(data, edge)
+%# index = assign1dbin(data, edge);
+%# [index, histogram] = assign1dbin(data, edge)
 %#
-%# index = assign1dbins(data, nbin);
-%# [index, center, edge] = assign1dbins(data, nbin)
-%
 %% Description
-% This routine assigns bin indices to the given data by binning
-% their values. Bins are numbered in acending order of the data
-% values. For example min(data) belongs to the 1st bin, and
-% max(data) belongs to the last bin.
+% This routine assigns bin index to the given data by binning
+% their values. 
 %
-% * data   - data to be assigned 
-%            [double nframe x 1]
-% * edge   - edges of bins
-%            [double nframe+1 x 1]
-% * index  - indices of bins.
-%            [integer nframe x 1]
-% * center - centers of bins
-%            [double nframe x 1]
-% * nbin   - the number of bins
-%            [integer scalar]
+% For each data(i), bin index k is assgined if data(i) is
+%   edge(k) ≤ data(i) < edge(k+1)
+% 
+% NaN is assigned to data outside the given edges.
+% Also, data outside the given edges are not counted in the
+% histogram. 
+%
+% * data - data to be assigned 
+%          [double nframe x 1]
+% * edge - edges of bins
+%          [double nedge x 1]
+%
+% * index     - bined index data
+%               [integer nframe x 1]
+% * histogram - binned histogram
+%               [integer nedge-1 x 1]
 %
 %% Example
 %# data = rand(100000, 2);
-%# [index, center] = assign1dbins(data(:, 1), 0:0.1:1);
+%# index = assign1dbins(data(:, 1), 0:0.1:1);
 %# scatter(data(:, 1), data(:, 2), 5, index, 'filled');
 % 
 %% See also
-% assign2dbins, assignvoronoi
+% assign2dbin, assignvoronoi
 %
 
 if isrow(data)
   data = data';
 end
+nedge = numel(edge);
 
-if ~exist('edge', 'var') || isempty(edge)
-  edge = 10;
-end
+[histogram, index] = histc(data, edge);
 
-if numel(edge) == 1
-  nbin = edge;
-  data_min = min(data(:, 1));
-  data_max = max(data(:, 1));
-  edge = linspace(data_min, data_max + nbin*eps, nbin+1);
-else
-  nbin = numel(edge) - 1;
-end
-
-[~, index] = histc(data, edge);
-center = 0.5*(edge(2:end) + edge(1:(end-1)));
+index(index == nedge) = NaN;
+index(index == 0) = NaN;
+histogram(nedge) = [];
 
