e38580baf905e8583c7808068c8f9f9643ae147c
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2015-09-24 11:26:04 +0900

    add msm routine

---
+++ mdtoolbox/msmget.m
@@ -0,0 +1,50 @@
+function trjOfState = msmget(state, indexOfCluster, trj)
+%% msmget
+% get physical quantieis from sequence of states
+%
+%% Syntax
+%# trjOfState = msmget(state, indexOfCluster, trj)
+%
+%% Description
+% 
+%
+%% Example
+%#
+% 
+%% See also
+%
+%% TODO
+% 
+%
+
+%% setup
+nframe = numel(state);
+trjOfState = zeros(nframe, 1);
+
+if iscell(indexOfCluster)
+  indexOfCluster2 = indexOfCluster;
+  indexOfCluster = [];
+  for i = 1:numel(indexOfCluster)
+    indexOfCluster = [indexOfCluster; indexOfCluster2{i}];
+  end
+  clear indexOfCluster2;
+end
+
+if iscell(trj)
+  trj2 = trj;
+  trj = [];
+  for i = 1:numel(trj)
+    trj = [trj; trj2{i}];
+  end
+  clear trj2;
+end
+
+%% get physical quantieis
+for iframe = 1:nframe
+  index = (indexOfCluster == state(iframe));
+  index = find(index);
+  nz = numel(index);
+  ri = randi(nz);
+  trjOfState(iframe) = indexOfCluster(index(ri));
+end
+
